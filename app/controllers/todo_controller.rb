2ccd978f2f2c95202a9cec501bfb231c42c843f2
Julee Nhek <j.nhek01@gmail.com>
2017-11-18 22:32:27 +0000

    add if statements in the todo list while numbering it.

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,9 +2,26 @@ class TodoController<ApplicationController
      def index
      end
      def show
-     @todo_description = "Make the curriculum"
+     @todo_description = "Are You Done?"
 @todo_pomodoro_estimate = 4
      end
-end
 
+def show
+    todo_index=params[:id]
+    
+    if todo_index=="1"
+        @todo_description="Eat"
+        @todo_time_estimate="1"
+    elsif todo_index=="2"
+        @todo_description="Ap Bio"
+        @todo_time_estimate="3"
+    elsif todo_index=="3"
+        @todo_description="Nap"
+        @todo_time_estimate="5"
+    elsif todo_index=="4"
+        @todo_description="Watch Youtube"
+        @todo_time_estimate="3"
+    end
+end
+end
 
