3252a8d9071eb6a4148fa99c6da5240e282d9c01
Julee Nhek <j.nhek01@gmail.com>
2017-11-19 19:06:33 +0000

    Add new RCAV flow

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,13 +1,9 @@
 class TodoController<ApplicationController
      def index
      end
-     def show
-     @todo_description = "Are You Done?"
-@todo_pomodoro_estimate = 4
-     end
-
 def show
     @todo = Todo.find_by_id(params[:id])
 end
+def new
 end
-
+end
