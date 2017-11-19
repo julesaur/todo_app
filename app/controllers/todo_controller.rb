2d3bde985a100c30640fe7798dd9e6a6f7b843bb
Julee Nhek <j.nhek01@gmail.com>
2017-11-19 19:49:18 +0000

    added a create method to add to the database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,16 @@
 class TodoController<ApplicationController
-     def index
-     end
-def show
-    @todo = Todo.find_by_id(params[:id])
-end
-def new
-end
+    def index
+    end
+    def show
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def new
+    end
+    def create
+        t = Todo.new
+        t.description=params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
 end
